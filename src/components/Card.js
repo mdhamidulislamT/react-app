e3e867fb653fb25e58fa92a2d9424fafe6a91a9c
mdhamidulislamT <hamidlive6@gmail.com>
2022-09-20 23:18:29 +0600

    useState with Object and Form controll

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,12 @@
-
-
-
-const Card=(props)=>{
-    const {txt, txt2} = props;
-    return(
-        <div>
-            <h1>This is test Card</h1>
-            <p>{txt}</p>
-            <h3>{txt2}</h3>
-        </div>
-    );
-}
+const Card = (props) => {
+  const { txt, txt2 } = props;
+  return (
+    <div>
+      <h1>This is test Card</h1>
+      <p>{txt}</p>
+      <h3>{txt2}</h3>
+    </div>
+  );
+};
 
 export default Card;
